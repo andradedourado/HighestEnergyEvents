ba1369fba6af1dd580b450121a44965305d63f20
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2024-12-22 17:51:32 -0300

    Add plot of arrival directions for the four highest-energy events

--- events/main.py
+++ events/main.py
@@ -108,9 +108,48 @@ def map_arrival_directions():
 	plt.show()
 
 # ----------------------------------------------------------------------------------------------------
+def map_arrival_directions_top4():
+	
+	data = np.genfromtxt('highest_energy_events.dat', dtype = None)
+
+	plt.figure()
+	axs = plt.subplot(111, projection = 'mollweide')
+	axs.set_longitude_grid_ends(90)
+	
+	l = np.zeros(len(data))
+	b = np.zeros(len(data))
+	E = np.zeros(len(data))
+
+	for idata in range(len(data)):
+		l[idata] = galactic_l(idata)
+		b[idata] = galactic_b(idata)
+		E[idata] = data[idata][1]
+
+	l = l[[1, 2, 3, 4]]
+	b = b[[1, 2, 3, 4]]
+	E = E[[1, 2, 3, 4]]
+
+	plt.fill(countours_auger_sky()[:,1], countours_auger_sky()[:,0], facecolor = 'lightgray', edgecolor = None)
+	plt.scatter(l, b, marker = 'D', c = E, cmap = 'viridis', s = 24, vmin = 46, vmax = 166)
+	plt.colorbar(label = r'Energy of the event$\:$[EeV]', location = 'bottom')
+
+	plt.xlabel(r'Galactic longitude, $l \: {\rm [deg]}$', labelpad = 10)
+	plt.ylabel(r'Galactic latitude, $b \: {\rm [deg]}$')
+	plt.xticks(ticks = [-120 * degree, -60 * degree, 0 * degree, 60 * degree, 120 * degree],
+	labels = [r'$120\degree$', r'$60\degree$', r'$0\degree$', r'$300\degree$', r'$240\degree$'])
+	plt.yticks(ticks = [-60 * degree, -30 * degree, 0 * degree, 30 * degree, 60 * degree], 
+	labels = [r'$-60\degree$', r'$-30\degree$', r'$0\degree$', r'$30\degree$', r'$60\degree$'])
+	plt.grid(linestyle = 'dotted', color = 'black', linewidth = 0.5, zorder = -1.0)
+	plt.savefig('highest_energy_events_top4.pdf', format = 'pdf', bbox_inches = 'tight')
+	plt.savefig('highest_energy_events_top4.png', format = 'png', bbox_inches = 'tight', dpi = 300)
+	
+	plt.show()
+
+# ----------------------------------------------------------------------------------------------------
 def main():
 
-	map_arrival_directions()
+	# map_arrival_directions()
+	map_arrival_directions_top4()
 
 # ----------------------------------------------------------------------------------------------------
 main()
