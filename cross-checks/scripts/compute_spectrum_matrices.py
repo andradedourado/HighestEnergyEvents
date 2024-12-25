ee24a3bd92758707b9515c6feb337752d4732ea7
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2024-12-24 21:24:16 -0300

    Compute spectra for UF24 parameters

--- cross-checks/scripts/compute_spectrum_matrices.py
+++ cross-checks/scripts/compute_spectrum_matrices.py
@@ -1,38 +1,30 @@
 import numpy as np
 
-cts = np.logspace(0, 3.5, num = 71)
-Es = np.logspace(0, 4, num = 81)
-Es = np.delete(Es, 0)
-
-particles = ['1H', '4He', '14N', '28Si', '56Fe']
-Zss = [1, 2, 7, 14, 26]
-
-gmms = [2.2, 2.7]
-
-dir = '/Users/andradedourado/Simulations/Runs/Analysis'
+BASE_DIR = '/Users/andradedourado/Simulations/Runs/Analysis'
+RESULTS_DIR = '../results'
+PARTICLES = ['1H', '4He', '14N', '28Si', '56Fe']
+ZSS = [1, 2, 7, 14, 26]
+GMMS = [2.2, 2.7]
+CTS = np.logspace(0, 3.5, num = 71)
+ES = np.delete(np.logspace(0, 4, num = 81), 0)
 
 # ----------------------------------------------------------------------------------------------------
 def w_sim(Es, cts): 
 
-	Es = Es * 1.e18
-	w = Es * cts
-
-	return w
+    return Es * 1.e18 * cts
 
 # ----------------------------------------------------------------------------------------------------
-def w_spec(Es, Zs, gmm): # No exponential cutoff
+def w_spec(Es, Zs, gmm, Rcut):
 
-	Es = Es*1.e18
-	w = Es**(-gmm) 
-	
-	return w
+    return (Es * 1.e18) ** -gmm * np.exp(-(Es * 1.e18) / (Zs * Rcut))
 
 # ----------------------------------------------------------------------------------------------------
 def iZs(Zs):
 
-    for iZs in range(len(Zss)):
-        if Zs == Zss[iZs]:
-            return iZs
+    try:
+        return ZSS.index(Zs)
+    except ValueError:
+        raise ValueError(f"Zs ({Zs}) não encontrado em ZSS.")
 
 # ----------------------------------------------------------------------------------------------------
 def gmm_filename_suffix(gmm):
@@ -43,43 +35,45 @@ def gmm_filename_suffix(gmm):
         return 'thin'
 
 # ----------------------------------------------------------------------------------------------------
-def compute_earth_spectrum_matrix(gmm, Zs):
+def save_spectrum_file(matrix, gmm, Zs, horizon, location):
 
-    matrix = np.zeros((len(cts), 79))
+    filename = f"{RESULTS_DIR}/{location}_spectrum_matrix_{horizon}.dat" \
+               if horizon == 'UF24' else \
+               f"{RESULTS_DIR}/{location}_spectrum_matrix_{PARTICLES[iZs(Zs)]}_{gmm_filename_suffix(gmm)}.dat"
 
-    for icts in range(len(cts)):
-
-        for iEs in range(len(Es)):
-
-            data = np.loadtxt(dir + '/{0}/S_ID{1:02d}D{2:02d}E0{3:02d}.dat'.format(particles[iZs(Zs)], iZs(Zs), icts, iEs))
-                
-            for idata in range(len(data)):
-                matrix[icts, idata] += data[idata] * w_spec(Es[iEs], Zs, gmm) * w_sim(Es[iEs], cts[icts])
-
-    np.savetxt('../results/earth_spectrum_matrix_{0}_{1}.dat'.format(particles[iZs(Zs)], gmm_filename_suffix(gmm)), matrix, fmt = '%e', header = 'gmm = {0} | Rmax = \infty V | Zs = {1}'.format(gmm, Zs), delimiter = '\t')
+    header = f"gmm = {gmm} | Rcut = {'∞' if horizon == 'HMR06' else '10^18.6 V'} | Zs = {Zs}"
+    
+    np.savetxt(filename, matrix, fmt = '%e', header = header, delimiter = '\t')
 
 # ----------------------------------------------------------------------------------------------------
-def compute_source_spectrum_matrix(gmm, Zs):
+def compute_matrix(gmm, Rcut, Zs, horizon, location, num_particles = 1000):
 
-    num_particles = 1000
+    matrix = np.zeros((len(CTS), len(ES) - 1))
 
-    matrix = np.zeros((len(cts), 79))
+    for icts, cts in enumerate(CTS):
+        for iEs, Es in enumerate(ES[:-1]):
+            weight = w_spec(Es, Zs, gmm, Rcut) * w_sim(Es, cts)
 
-    for icts in range(len(cts)):
+            if location == 'earth':
+                data = np.loadtxt(f"{BASE_DIR}/{PARTICLES[iZs(Zs)]}/S_ID{iZs(Zs):02d}D{icts:02d}E0{iEs:02d}.dat")
+                matrix[icts] += data * weight
+            elif location == 'source':
+                matrix[icts, iEs] += num_particles * weight
 
-        for iEs in range(len(Es)-1):
-            
-            matrix[icts, iEs] += num_particles * w_spec(Es[iEs], Zs, gmm) * w_sim(Es[iEs], cts[icts])
-
-    np.savetxt('../results/source_spectrum_matrix_{0}_{1}.dat'.format(particles[iZs(Zs)], gmm_filename_suffix(gmm)), matrix, fmt = '%e', header = 'gmm = {0} | Rmax = \infty V | Zs = {1}'.format(gmm, Zs), delimiter = '\t')
+    save_spectrum_file(matrix, gmm, Zs, horizon, location)
 
 # ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    for gmm in gmms: 
-        for Zs in Zss:
-            if Zs not in [2, 7]:
-                compute_earth_spectrum_matrix(gmm, Zs)
-                compute_source_spectrum_matrix(gmm, Zs)
+    # HMR06
+    # for gmm in GMMS:
+    #     for Zs in ZSS:
+    #         if Zs not in [2, 7]:
+    #             compute_matrix(gmm, np.inf, Zs, 'HMR06', 'earth')
+    #             compute_matrix(gmm, np.inf, Zs, 'HMR06', 'source')
+
+    # UF24
+    compute_matrix(1.0, 10**18.6, 26, 'UF24', 'earth')
+    compute_matrix(1.0, 10**18.6, 26, 'UF24', 'source')
 
 # ----------------------------------------------------------------------------------------------------
