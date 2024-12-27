b2b4b7e4d775fffdbff743920309db2ea6d83da5
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2024-12-26 22:21:51 -0300

    Add plot for comparison with UF24 results

--- cross-checks/scripts/plot_UF24_cross-checks.py
+++ cross-checks/scripts/plot_UF24_cross-checks.py
@@ -24,8 +24,26 @@ def plot_UF24_original():
     plt.show()
 
 # ----------------------------------------------------------------------------------------------------
+def plot_UF24_comparison():
+
+    data_UF24 = np.loadtxt('../data/UF24_original.dat')
+    data_LAD = np.loadtxt('../results/UF24_definition_cut.dat')
+    plt.plot(data_UF24[:,0]*100, data_UF24[:,1], color = 'k', label = 'UF24') 
+    plt.plot(data_LAD[:,0], data_LAD[:,1], color = 'r', label = 'LAD')
+    plt.yscale('log')
+    plt.xlim([100, 300])
+    plt.ylim([0, 200])
+    plt.xlabel(r'Energy$\: \rm [EeV]$')
+    plt.ylabel(r'Distance$\: \rm [Mpc]$')
+    plt.legend(title = r'Results')
+    plt.savefig('../figures/UF24_comparison.pdf', bbox_inches = 'tight')
+    plt.savefig('../figures/UF24_comparison.png', bbox_inches = 'tight', dpi = 300)
+    plt.show()
+
+# ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    plot_UF24_original()
+    # plot_UF24_original()
+    plot_UF24_comparison()
 
 # ----------------------------------------------------------------------------------------------------
