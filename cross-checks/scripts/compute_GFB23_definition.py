b5945966d963095ca9e290bd30884caac5ccaefb
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2024-12-30 20:28:42 -0300

    Compare my implementation with GFB23 benchmark results

--- cross-checks/scripts/compute_GFB23_definition.py
+++ cross-checks/scripts/compute_GFB23_definition.py
@@ -3,6 +3,7 @@ import numpy as np
 RESULTS_DIR = '../results'
 PARTICLES = ['1H', '14N', '56Fe']
 ZSS = [1, 14, 56]
+EOBS = [150., 300.]
 CTS = np.logspace(0, 3.5, num = 71)
 ES = np.delete(np.logspace(0, 4, num = 81), 0)
 E = np.logspace(4./(2.*(len(ES) - 1)), 4. - 4./(2.*(len(ES) - 1)), num = len(ES) - 1)
@@ -18,8 +19,8 @@ def iZs(Zs):
 # ----------------------------------------------------------------------------------------------------
 def compute_GFB23_definition_matrix(Zs):
 
-    data_earth = np.loadtxt(f"{RESULTS_DIR}/earth_spectrum_matrix_{PARTICLES[iZs(Zs)]}_3ZeV.dat")
-    data_source = np.loadtxt(f"{RESULTS_DIR}/source_spectrum_matrix_{PARTICLES[iZs(Zs)]}_3ZeV.dat")
+    earth_spectrum = np.loadtxt(f"{RESULTS_DIR}/earth_spectrum_matrix_{PARTICLES[iZs(Zs)]}_3ZeV.dat")
+    source_spectrum = np.loadtxt(f"{RESULTS_DIR}/source_spectrum_matrix_{PARTICLES[iZs(Zs)]}_3ZeV.dat")
 
     matrix_num = np.zeros((len(CTS), len(ES) - 1))
     matrix_den = np.zeros((len(CTS), len(ES) - 1))
@@ -27,19 +28,38 @@ def compute_GFB23_definition_matrix(Zs):
     for icts in range(len(CTS)):
         for iE in range(len(E)):
             for iiE in range(iE, len(E)):
-                matrix_num[icts, iE] += data_earth[icts, iiE]
-                matrix_den[icts, iE] += data_source[icts, iiE]
+                matrix_num[icts, iE] += earth_spectrum[icts, iiE]
+                matrix_den[icts, iE] += source_spectrum[icts, iiE]
 
     matrix = matrix_num / matrix_den
     
-    filename = f"{RESULTS_DIR}/GFB23_definition_matrix_{PARTICLES[iZs(Zs)]}_3ZeV.dat"
+    filename = f"{RESULTS_DIR}/GFB23_definition_matrix_{PARTICLES[iZs(Zs)]}.dat"
     header = f"gmm = 2.5 | Rcut = 3.e21 / {Zs} V | Zs = {Zs}"
     np.savetxt(filename, matrix, fmt = '%e', header = header, delimiter = '\t')
+    
+# ----------------------------------------------------------------------------------------------------
+def compute_GFB23_definiton_aGZK(Zs, Eobs):
+
+    data = np.loadtxt(f"{RESULTS_DIR}/GFB23_definition_matrix_{PARTICLES[iZs(Zs)]}.dat")
+
+    for iE in range(len(E)):
+        if Eobs >= E[iE] and Eobs < E[iE + 1]:
+            break
+
+    f = open(f"{RESULTS_DIR}/GFB23_definition_aGZK_{PARTICLES[iZs(Zs)]}_{int(Eobs)}EeV.dat", 'w')
+    f.write(f'# [{round(E[iE])}, {round(E[iE + 1])}] EeV\n')    
+    for icts, cts in enumerate(CTS):
+        f.write(f'{cts:.15e}\t{data[icts, iE]:.15e}\t{data[icts, iE + 1]:.15e}\n')
+    f.close()
     
 # ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
     for Zs in ZSS:
         compute_GFB23_definition_matrix(Zs)
+    
+    for Zs in ZSS:
+        for Eobs in EOBS:
+            compute_GFB23_definiton_aGZK(Zs, Eobs)
 
 # ----------------------------------------------------------------------------------------------------
--- cross-checks/scripts/plot_GFB23_cross-checks.py
+++ cross-checks/scripts/plot_GFB23_cross-checks.py
@@ -1,4 +1,3 @@
-from matplotlib import lines
 from matplotlib.offsetbox import AnchoredText
 from matplotlib.pylab import cm
 import matplotlib.pyplot as plt
@@ -14,6 +13,7 @@ plt.rcParams.update({'legend.fontsize': 'large',
 PARTICLES = ['1H', '14N', '56Fe']
 PARTICLES_LEGEND = [r'$^1\mathrm{H}$', r'$^{14}\mathrm{N}$', r'$^{56}\mathrm{Fe}$']
 ZSS = [1, 7, 26]
+EOBS = [150., 300.]
 
 # ----------------------------------------------------------------------------------------------------
 def iZs(Zs):
@@ -26,7 +26,7 @@ def iZs(Zs):
 def plot_GFB23_original(Eobs):
 
     for Zs in ZSS:
-        data = np.loadtxt(f'../data/GFB23_original_{PARTICLES[iZs(Zs)]}_{int(Eobs)}EeV.dat') 
+        data = np.loadtxt(f"../data/GFB23_original_{PARTICLES[iZs(Zs)]}_{int(Eobs)}EeV.dat") 
         plt.plot(data[:,0], data[:,1], label = r'{0}'.format(PARTICLES_LEGEND[iZs(Zs)]))
 
     at = AnchoredText(r'GRB23 | {0}$\: \rm EeV$'.format(int(Eobs)), loc = 'upper right', frameon = False, prop = {'fontsize': 'large'})
@@ -36,14 +36,38 @@ def plot_GFB23_original(Eobs):
     plt.xlabel(r'Source distance$\: \rm [Mpc]$')
     plt.ylabel(r'$a_{\rm GZK}$')
     plt.legend(frameon = False, loc = 'lower left')
-    plt.savefig(f'../figures/GFB23_original_{int(Eobs)}EeV.pdf', bbox_inches = 'tight')
-    plt.savefig(f'../figures/GFB23_original_{int(Eobs)}EeV.png', bbox_inches = 'tight', dpi = 300)
+    plt.savefig(f"../figures/GFB23_original_{int(Eobs)}EeV.pdf", bbox_inches = 'tight')
+    plt.savefig(f"../figures/GFB23_original_{int(Eobs)}EeV.png", bbox_inches = 'tight', dpi = 300)
+    plt.show()
+
+# ----------------------------------------------------------------------------------------------------
+def plot_GFB23_comparison(Zs, Eobs):
+
+    data_GFB23 = np.loadtxt(f"../data/GFB23_original_{PARTICLES[iZs(Zs)]}_{int(Eobs)}EeV.dat") 
+    data_LAD = np.loadtxt(f"../results/GFB23_definition_aGZK_{PARTICLES[iZs(Zs)]}_{int(Eobs)}EeV.dat")
+    plt.plot(data_GFB23[:,0], data_GFB23[:,1], label = 'GFB23', color = 'k')
+    plt.fill_between(data_LAD[:,0], data_LAD[:,1], data_LAD[:,2], label = 'LAD', color = 'k', alpha = 0.25)
+    
+    at = AnchoredText(r'GRB23 | {0} | {1}$\: \rm EeV$'.format(PARTICLES_LEGEND[iZs(Zs)], int(Eobs)), loc = 'upper center', frameon = False, prop = {'fontsize': 'large'})
+    plt.gca().add_artist(at)
+
+    plt.xscale('log')
+    plt.xlim([1.e0, 1.e2])
+    plt.xlabel(r'Source distance$\: \rm [Mpc]$')
+    plt.ylabel(r'$a_{\rm GZK}$')
+    plt.legend(title = 'Results')
+    plt.savefig(f"../figures/GFB23_comparison_{PARTICLES[iZs(Zs)]}_{int(Eobs)}EeV.pdf", bbox_inches = 'tight')
+    plt.savefig(f"../figures/GFB23_comparison_{PARTICLES[iZs(Zs)]}_{int(Eobs)}EeV.png", bbox_inches = 'tight', dpi = 300)
     plt.show()
 
 # ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
     # plot_GFB23_original(150.)
-    plot_GFB23_original(300.)
+    # plot_GFB23_original(300.)
+    
+    for Eobs in EOBS:
+        for Zs in ZSS:
+            plot_GFB23_comparison(Zs, Eobs)
 
 # ----------------------------------------------------------------------------------------------------
