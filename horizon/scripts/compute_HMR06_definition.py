c75888ab51f1bfc0116ec50944bbacc78e3ce262
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-01-01 11:46:32 -0300

    Standardize files

--- horizon/scripts/compute_HMR06_definition.py
+++ horizon/scripts/compute_HMR06_definition.py
@@ -1,24 +1,21 @@
 import numpy as np
 import sys
 
-cts = np.logspace(0, 3.5, num = 71)
-Es = np.logspace(0, 4, num = 81)
-Es = np.delete(Es, 0)	
-E = np.logspace(4./(2.*79), 4. - 4./(2.*79), num = 79) 
-
-particles = ['1H', '4He', '14N', '28Si', '56Fe']
-Zss = [1, 2, 7, 14, 26]
-
-gmm = 1.
-Rmax = 10**18.6 # V
-
-fractions = [0.05, 0.1]
+RESULTS_DIR = '../results'
+PARTICLES = ['1H', '4He', '14N', '28Si', '56Fe']
+ZSS = [1, 2, 7, 14, 26]
+FRACTIONS = [0.05, 0.1]
+CTS = np.logspace(0, 3.5, num = 71)
+ES = np.delete(np.logspace(0, 4, num = 81), 0)
+E = np.logspace(4./(2.*(len(ES) - 1)), 4. - 4./(2.*(len(ES) - 1)), num = len(ES) - 1)
+GMM = 1.
+RCUT = 10**18.6 # V
 
 # ----------------------------------------------------------------------------------------------------
 def iZs(Zs):
 
-    for iZs in range(len(Zss)):
-        if Zs == Zss[iZs]:
+    for iZs in range(len(ZSS)):
+        if Zs == ZSS[iZs]:
             return iZs
         
 # ----------------------------------------------------------------------------------------------------
@@ -32,43 +29,41 @@ def EGMF(is_EGMF):
 # ----------------------------------------------------------------------------------------------------
 def compute_HMR06_definition_matrix(Zs, is_EGMF):
 
-    data = np.loadtxt('../results/intensity_matrix_{0}_{1}.dat'.format(particles[iZs(Zs)], EGMF(is_EGMF)))
+    data = np.loadtxt(f"{RESULTS_DIR}/earth_spectrum_matrix_{PARTICLES[iZs(Zs)]}_{EGMF(is_EGMF)}.dat")
 
-    matrix = np.zeros((len(cts), 79))
+    matrix = np.zeros((len(CTS), len(ES) - 1))
 
     for iE in range(len(E)):
-        for icts in range(len(cts)):
+        for icts in range(len(CTS)):
             matrix[icts, iE] = np.sum(data[icts:, iE:])
 
     matrix[0, matrix[0,:] == 0] = sys.float_info.min
     matrix = matrix / matrix[0,:]
     
-    np.savetxt('../results/HMR06_definition_matrix_{0}_{1}.dat'.format(particles[iZs(Zs)], EGMF(is_EGMF)), matrix, fmt = '%e', header = 'gmm = {0} | Rmax = 10^{{{1}}} V | Zs = {2}'.format(gmm, np.log10(Rmax), Zs), delimiter = '\t')
+    np.savetxt(f"{RESULTS_DIR}/HMR06_definition_matrix_{PARTICLES[iZs(Zs)]}_{EGMF(is_EGMF)}.dat", matrix, fmt = '%e', header = 'gmm = {0} | Rcut = 10^{{{1}}} V | Zs = {2}'.format(GMM, np.log10(RCUT), Zs), delimiter = '\t')
 
 # ----------------------------------------------------------------------------------------------------
 def compute_HMR06_definition_cut(Zs, is_EGMF, fraction):
 
-    data = np.loadtxt('../results/HMR06_definition_matrix_{0}_{1}.dat'.format(particles[iZs(Zs)], EGMF(is_EGMF)))
+    data = np.loadtxt(f"{RESULTS_DIR}/HMR06_definition_matrix_{PARTICLES[iZs(Zs)]}_{EGMF(is_EGMF)}.dat")
 
-    f = open('../results/HMR06_definition_cut_{0}_{1}_{2:02d}.dat'.format(particles[iZs(Zs)], EGMF(is_EGMF), int(fraction * 100)), 'w')
+    f = open(f"{RESULTS_DIR}/HMR06_definition_cut_{PARTICLES[iZs(Zs)]}_{EGMF(is_EGMF)}_{int(fraction * 100):02d}.dat", 'w')
 
-    for iE in range(79):
-        for icts in range(len(cts)):
+    for iE in range(len(ES) - 1):
+        for icts in range(len(CTS)):
             if data[icts,iE] < fraction:
                 break 
         f.write(str('{:.15e}'.format(E[iE])) + '\t')
-        f.write(str('{:.15e}'.format(cts[icts])) + '\n')
+        f.write(str('{:.15e}'.format(CTS[icts])) + '\n')
 
     f.close()
 
 # ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    is_EGMF = False
-
-    for Zs in Zss:
-        compute_HMR06_definition_matrix(Zs, is_EGMF)
-        for fraction in fractions:
-            compute_HMR06_definition_cut(Zs, is_EGMF, fraction)
+    for Zs in ZSS:
+        compute_HMR06_definition_matrix(Zs, False)
+        for fraction in FRACTIONS:
+            compute_HMR06_definition_cut(Zs, False, fraction)
 
 # ----------------------------------------------------------------------------------------------------
--- horizon/scripts/compute_spectrum_matrices.py
+++ horizon/scripts/compute_spectrum_matrices.py
@@ -54,14 +54,40 @@ def compute_intensity_matrix(gmm, Rmax, Zs, is_EGMF):
     np.savetxt(f"{RESULTS_DIR}/intensity_matrix_{PARTICLES[iZs(Zs)]}_{EGMF(is_EGMF)}.dat", matrix, fmt = '%e', header = 'gmm = {0} | Rmax = 10^{{{1}}} V | Zs = {2}'.format(gmm, np.log10(Rmax), Zs), delimiter = '\t')
 
 # ----------------------------------------------------------------------------------------------------
+def save_spectrum_file(matrix, Zs, location, is_EGMF):
+
+    if location == 'source':
+        filename = f"{RESULTS_DIR}/{location}_spectrum_matrix_{PARTICLES[iZs(Zs)]}.dat"
+    elif location == 'earth':
+        filename = f"{RESULTS_DIR}/{location}_spectrum_matrix_{PARTICLES[iZs(Zs)]}_{EGMF(is_EGMF)}.dat"
+
+    np.savetxt(filename, matrix, fmt = '%e', delimiter = '\t')
+
+# ----------------------------------------------------------------------------------------------------
+def compute_matrix(gmm, Rcut, Zs, location, is_EGMF, num_particles = 1000):
+
+    matrix = np.zeros((len(CTS), len(ES) - 1))
+
+    for icts, cts in enumerate(CTS):
+        for iEs, Es in enumerate(ES[:-1]):
+            weight = w_spec(Es, Zs, gmm, Rcut) * w_sim(Es, cts)
+
+            if location == 'earth':
+                data = np.loadtxt(f"{BASE_DIR}/{PARTICLES[iZs(Zs)]}/S_ID{iZs(Zs):02d}D{icts:02d}E0{iEs:02d}.dat")
+                if is_EGMF == False:
+                    matrix[icts] += data * weight
+                # elif is_EGMF == True:
+            elif location == 'source':
+                matrix[icts, iEs] += num_particles * weight
+
+    save_spectrum_file(matrix, Zs, location, is_EGMF)
+
+# ----------------------------------------------------------------------------------------------------  
 if __name__ == '__main__':
 
-    gmm = 1.
-    Rmax = 10**18.6 # V
-
-    is_EGMF = False
-
     for Zs in ZSS:
-        compute_intensity_matrix(gmm, Rmax, Zs, is_EGMF)
+        compute_matrix(1., 10**18.6, Zs, 'earth', False)
+        compute_matrix(1., 10**18.6, Zs, 'source', False)
+
 
 # ----------------------------------------------------------------------------------------------------
