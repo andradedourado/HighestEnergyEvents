209a11e4cc5a4f1cb0b011f40b3186adee89c4ef
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-01-01 19:57:28 -0300

    Minor corrections

--- horizon/scripts/compute_spectrum_matrices.py
+++ horizon/scripts/compute_spectrum_matrices.py
@@ -34,26 +34,6 @@ def EGMF(is_EGMF):
         return 'EGMF'
 
 # ----------------------------------------------------------------------------------------------------
-def compute_intensity_matrix(gmm, Rmax, Zs, is_EGMF):
-
-    matrix = np.zeros((len(CTS), 79))
-
-    if is_EGMF == False:
-
-        for icts in range(len(CTS)):
-
-            for iEs in range(len(ES)):
-
-                data = np.loadtxt(dir + '/{0}/S_ID{1:02d}D{2:02d}E0{3:02d}.dat'.format(PARTICLES[iZs(Zs)], iZs(Zs), icts, iEs))
-                
-                for idata in range(len(data)):
-                    matrix[icts, idata] += data[idata] * w_spec(ES[iEs], Zs, gmm, Rmax) * w_sim(ES[iEs], CTS[icts])
-
-    # elif is_EGMF == True:
-
-    np.savetxt(f"{RESULTS_DIR}/intensity_matrix_{PARTICLES[iZs(Zs)]}_{EGMF(is_EGMF)}.dat", matrix, fmt = '%e', header = 'gmm = {0} | Rmax = 10^{{{1}}} V | Zs = {2}'.format(gmm, np.log10(Rmax), Zs), delimiter = '\t')
-
-# ----------------------------------------------------------------------------------------------------
 def save_spectrum_file(matrix, Zs, location, is_EGMF):
 
     if location == 'source':
@@ -88,6 +68,5 @@ if __name__ == '__main__':
     for Zs in ZSS:
         compute_matrix(1., 10**18.6, Zs, 'earth', False)
         compute_matrix(1., 10**18.6, Zs, 'source', False)
-
 
 # ----------------------------------------------------------------------------------------------------
