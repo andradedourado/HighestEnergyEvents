a22b8796b0def79c91726834d3635d95982aa85b
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-01-03 22:48:22 -0300

    Adjust HRM06 horizon computation for cross-check validation

--- cross-checks/scripts/compute_HMR06_definition.py
+++ cross-checks/scripts/compute_HMR06_definition.py
@@ -32,7 +32,6 @@ def gmm_filename_suffix(gmm):
 def compute_HMR06_definition_matrix(gmm, Zs):
 
     data = np.loadtxt('../results/earth_spectrum_matrix_{0}_{1}.dat'.format(particles[iZs(Zs)], gmm_filename_suffix(gmm)))
-    data = data / np.loadtxt('../results/source_spectrum_matrix_{0}_{1}.dat'.format(particles[iZs(Zs)], gmm_filename_suffix(gmm)))
 
     matrix = np.zeros((len(cts), 79))
 
